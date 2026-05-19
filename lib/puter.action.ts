43e0f31ea49753ab09bf593691edefcb77ced660
Ariez <kritamet.cho@gmail.com>
2026-05-20 03:55:03 +0700

    fix bug

--- lib/puter.action.ts
+++ lib/puter.action.ts
@@ -14,7 +14,7 @@ export const getCurrentUser = async () => {
   }
 }
 
-export const createProject = async({ item }: CreateProjectParams): Promise<DesignItem | null | undefined> => {
+export const createProject = async ({ item }: CreateProjectParams): Promise<DesignItem | null | undefined> => {
   const projectId = item.id;
 
   const hosting = await getOrCreateHostingConfig();
@@ -47,13 +47,13 @@ export const createProject = async({ item }: CreateProjectParams): Promise<Desig
     publicPath: _publicPath,
     ...rest
   } = item;
-  
+
   const payload = {
     ...rest,
     sourceImage: resolveSource,
     renderedImage: resolveRendered,
   }
-  
+
   try {
     // Call the Puter worker to store project in kv
 
